5c0907c35e6ab941ae6f2f7d554c091c0b7f89d9
Jelly <110519070+stars2302@users.noreply.github.com>
2023-10-04 23:37:21 +0900

    [FEAT] 전체적인 script 기능 추가
    scroll에 따른 class 추가 및 삭제,
    top button 클릭이벤트,
    skill progress,
    aos 라이브러리 연결

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
+/* top button */
+let topBtn = $('.top_btn');
+topBtn.click(function(e){
+  e.preventDefault();
+  $('body,html').animate({scrollTop:0},500);
+});
+
+
+/* skill progress */
+let skill = $('.skill_list li');
+let barWrap = $('.progress .bar_wrap');
+let bar = barWrap.find('.bar');
+let barCount = barWrap.find('.count');
+
+skill.click(function(){
+  let rate = $(this).attr('data-rate');
+  $({rate:0}).animate({rate:rate},{
+    duration: 1500,
+    progress:function(){
+      let now = Math.ceil(this.rate);
+      barCount.text(now+'%');
+      bar.css({width:`${now}%`});
+    }
+  })
+});
+
+
+
+
+/* site swiper slide */
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
@@ -10,6 +40,33 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+});
+
+/* AOS scroll */
+AOS.init({
+  duration: 1000,
+  offset: 700
+});
 
 
+/* header shrink, aside buttons show, skill animation */
+let header = $('.header');
+let asideBtns = $('.aside_btns');
+let myswitch = false;
+$(window).scroll(function(){
+  let sct = $(this).scrollTop();
+  if(sct >= header.offset().top){
+    header.addClass('shrink');
+    asideBtns.addClass('show');
+  } else{
+    header.removeClass('shrink');
+    asideBtns.removeClass('show');
+  }
+
+  if(sct >= skill.offset().top-500){
+    if(!myswitch){
+      skill.eq(0).trigger('click');
+      myswitch=true;
+    }
+  }
 });
